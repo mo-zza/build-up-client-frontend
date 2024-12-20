24264debc99cf69ecac0e480321b67b878e92924
mo-zza <bigsmalljoe@gmail.com>
2024-12-20 09:59:32 +0900

    feat: add prototype in project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {Header} from "@src/components/layout/header/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Header />
+      {/*<Header />*/}
         {children}
       </body>
     </html>
